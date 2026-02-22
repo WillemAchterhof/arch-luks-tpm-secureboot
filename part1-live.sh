9b0432473776293665e69977c53a3630d6813180
willemachterhof <willem.achterhof@protonmail.com>
2026-02-22 19:16:36 +0100

    Saver to change /* to /. for empty folders

--- part1-live.sh
+++ part1-live.sh
@@ -140,7 +140,7 @@ cleanup() {
     umount -R /mnt 2>/dev/null ||true
     cryptsetup close cryptroot 2>/dev/null || true
 }
-trap cleanup EXIT
+trap 'cleanup' ERR
 
 # ------------------------------------------------------------------------------
 # DISPLAY LUKS PASSWORD
@@ -195,13 +195,13 @@ mkdir -p /mnt/install/configs/themes/hyprland
 SCRIPT_BASE = "/run/media/arch/scripts"
 
 cp -r  "$SCRIPT_BASE"/install/part*.sh           /    /mnt/install/
-cp -r  "$SCRIPT_BASE"/configs/system/*           /    /mnt/install/configs/system/
-cp -r  "$SCRIPT_BASE"/configs/shell/*            /    /mnt/install/configs/shell/
-cp -r  "$SCRIPT_BASE"/configs/editor/*           /    /mnt/install/configs/editor/
-cp -r  "$SCRIPT_BASE"/configs/themes/waybar/*    /    /mnt/install/configs/themes/waybar/
-cp -r  "$SCRIPT_BASE"/configs/themes/rofi/*      /    /mnt/install/configs/themes/rofi/
-cp -r  "$SCRIPT_BASE"/configs/themes/alacritty/* /    /mnt/install/configs/themes/alacritty/
-cp -r  "$SCRIPT_BASE"/configs/themes/hyprland/*  /    /mnt/install/configs/themes/hyprland/
+cp -r  "$SCRIPT_BASE"/configs/system/.           /    /mnt/install/configs/system/
+cp -r  "$SCRIPT_BASE"/configs/shell/.            /    /mnt/install/configs/shell/
+cp -r  "$SCRIPT_BASE"/configs/editor/.           /    /mnt/install/configs/editor/
+cp -r  "$SCRIPT_BASE"/configs/themes/waybar/.    /    /mnt/install/configs/themes/waybar/
+cp -r  "$SCRIPT_BASE"/configs/themes/rofi/.      /    /mnt/install/configs/themes/rofi/
+cp -r  "$SCRIPT_BASE"/configs/themes/alacritty/. /    /mnt/install/configs/themes/alacritty/
+cp -r  "$SCRIPT_BASE"/configs/themes/hyprland/.  /    /mnt/install/configs/themes/hyprland/
 
 # ------------------------------------------------------------------------------
 # CPU DETECTION
