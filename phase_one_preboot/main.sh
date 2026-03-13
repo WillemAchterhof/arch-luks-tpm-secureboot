9ac9c8006f11ca69270b7d44a15cfb43c5443fa2
willemachterhof <willem.achterhof@protonmail.com>
2026-03-13 13:03:46 +0100

    again 5

--- phase_one_preboot/main.sh
+++ phase_one_preboot/main.sh
@@ -1,7 +1,3 @@
 #!usr/bin/env bash
 echo "Phase one preboot present."
 exec bash "$(dirname "${BASH_SOUCE[0]}")/../phase_two_postboot/main.sh
-
-
-exec bash "$(dirname "${BASH_SOUCE[0]}")/../configs/main.sh
-exec bash "$(dirname "${BASH_SOUCE[0]}")/../lib/main.sh
