8a628d15cb12e3228689eeb92fc76e3e1003a7a7
willemachterhof <willem.achterhof@protonmail.com>
2026-03-13 13:26:54 +0100

    hardcode the path

--- phase_one_preboot/main.sh
+++ phase_one_preboot/main.sh
@@ -1,7 +1,8 @@
 #!usr/bin/env bash
 echo "Phase one preboot present."
-exec bash "$(dirname "${BASH_SOUCE[0]}")/../phase_two_postboot/main.sh"
-exec bash "$(dirname "${BASH_SOUCE[0]}")/../phase_three_desktop/main.sh"
-exec bash "$(dirname "${BASH_SOUCE[0]}")/../phase_four_software/main.sh"
-exec bash "$(dirname "${BASH_SOUCE[0]}")/../configs/main.sh"
-exec bash "$(dirname "${BASH_SOUCE[0]}")/../lib/main.sh"
+DIR=/run/sa/arch-secure/
+exec bash "DIR/phase_two_postboot/main.sh"
+exec bash "DIR/phase_three_desktop/main.sh"
+exec bash "DIR/phase_four_software/main.sh"
+exec bash "DIR/configs/main.sh"
+exec bash "DIR/lib/main.sh"
