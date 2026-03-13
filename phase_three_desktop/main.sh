32be45770f01d60e22956e99d13321ef6af7cb83
willemachterhof <willem.achterhof@protonmail.com>
2026-03-13 16:03:08 +0100

    sight

--- phase_three_desktop/main.sh
+++ phase_three_desktop/main.sh
@@ -1,5 +1,5 @@
 echo "Phase three desktop present."
 
-
+DIR=/run/sa/arch-secure/
 chmod +x "$DIR/phase_four_software/main.sh"
 exec bash "$DIR/phase_four_software/main.sh"
--- phase_two_postboot/main.sh
+++ phase_two_postboot/main.sh
@@ -1,7 +1,7 @@
 #!usr/bin/env bash
 echo "Phase two postboot present."
 
-
+DIR=/run/sa/arch-secure/
 chmod +x "$DIR/phase_three_desktop/main.sh"
 exec bash "$DIR/phase_three_desktop/main.sh"
 
