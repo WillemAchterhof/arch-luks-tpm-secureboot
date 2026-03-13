37fb46d3a8f131e874b8166679d77eed3ad04eda
willemachterhof <willem.achterhof@protonmail.com>
2026-03-13 12:53:45 +0100

    take two

--- phase_one_preboot/main.sh
+++ phase_one_preboot/main.sh
@@ -1,7 +1,7 @@
 #!usr/bin/env bash
 echo "Phase one preboot present."
 exec bash "$(dirnam "${BASH_SOUCE[0]}")/../phase_two_postboot/main.sh
-exec bash "$(dirnam "${BASH_SOUCE[0]}")/../phase_three_destop/main.sh
-exec bash "$(dirnam "${BASH_SOUCE[0]}")/../phase__four_software/main.sh
+exec bash "$(dirnam "${BASH_SOUCE[0]}")/../phase_three_desktop/main.sh
+exec bash "$(dirnam "${BASH_SOUCE[0]}")/../phase_four_software/main.sh
 exec bash "$(dirnam "${BASH_SOUCE[0]}")/../configs/main.sh
 exec bash "$(dirnam "${BASH_SOUCE[0]}")/../lib/main.sh
