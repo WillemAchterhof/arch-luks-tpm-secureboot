f06ce2f99f9b67b299e40315fb193ebbcdb7b909
willemachterhof <willem.achterhof@protonmail.com>
2026-03-13 13:31:19 +0100

    I need a break

--- phase_one_preboot/main.sh
+++ phase_one_preboot/main.sh
@@ -1,8 +1,8 @@
 #!usr/bin/env bash
 echo "Phase one preboot present."
 DIR=/run/sa/arch-secure/
-exec bash "DIR/phase_two_postboot/main.sh"
-exec bash "DIR/phase_three_desktop/main.sh"
-exec bash "DIR/phase_four_software/main.sh"
-exec bash "DIR/configs/main.sh"
-exec bash "DIR/lib/main.sh"
+exec bash "$DIR/phase_two_postboot/main.sh"
+exec bash "$DIR/phase_three_desktop/main.sh"
+exec bash "$DIR/phase_four_software/main.sh"
+exec bash "$DIR/configs/main.sh"
+exec bash "$DIR/lib/main.sh"
